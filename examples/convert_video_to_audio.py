f8442534c26b16a6b8e1dde417effe7eebd1cc43
lewv <23514785+lewv@users.noreply.github.com>
2022-03-28 00:43:46 -0700

    Add files via upload
    run my_downloader before convert

---
+++ examples/convert_video_to_audio.py
@@ -0,0 +1,19 @@
+import os
+import sys
+from moviepy.editor import VideoFileClip
+
+def convert_video_to_audio_moviepy(video_file, output_ext="mp3"):
+    """Video to audio converter uses the MoviePy library
+    which uses `ffmpeg` (must be installed)"""
+    filename, ext = os.path.splitext(video_file)
+    clip = VideoFileClip(video_file)
+    clip.audio.write_audiofile(f"{filename}.{output_ext}")
+
+
+if __name__ == "__main__":
+    vf = sys.argv[1]
+    convert_video_to_audio_moviepy(vf)
+
+""" Use example
+$ python convert_video_to_audio.py zoo.webm
+"""
