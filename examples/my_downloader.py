05b0b696450a9effbef3901f377db830dcce8183
lewv <23514785+lewv@users.noreply.github.com>
2022-03-29 22:54:15 -0700

    Update my_downloader.py
    made it simpler for now

--- examples/my_downloader.py
+++ examples/my_downloader.py
@@ -1,39 +1,22 @@
 import pytube
-import validators
-import requests
 
 """
-my_downloader.py 03-21-2022
+my_downloader.py 03-30-2022
 runs at the command line
 downloads YouTube videos
-can run convert_video_to_audio.py afterwards 
 """
 
-
-
 url = input('Please enter a YouTube address:')
-valid = validators.url(url)
-verify = requests.get(url)
 
-if valid == True and verify.status_code < 400:
-  print('Just a moment please')
-  try:
-    pytube.YouTube(url).streams.get_highest_resolution().download()
-    print('Download is complete')
-  except:
-    print("An exception occurred, probably badly formed URL")
-elif valid == True and verify.status_code >= 400:
-  print('Not a valid URL')
-else:
-  print('wanted an else...')
-  
-
-
+pytube.YouTube(url).streams.get_highest_resolution().download()
+print('Download is complete')
 
 ### to try it type:
 ### python my_downloader.py
 ### and give it a valid YouTube address
 
-""" can try the following examples """
-""" https://www.youtube.com/watch?v=Tf-XPGneZEw """
-"""https://www.pipsnacks.com/404"""
+""" 
+You can try the following example 
+https://www.youtube.com/watch?v=LStXdttFj_o
+"""
+
