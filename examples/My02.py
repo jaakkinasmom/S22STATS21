aee3b4cf649ebd444c09bd9510e668f33449c07d
lewv <23514785+lewv@users.noreply.github.com>
2022-03-31 20:27:06 -0700

    Update My02.py
    added the url used in class

--- examples/My02.py
+++ examples/My02.py
@@ -2,6 +2,7 @@
 # need to install pytube for this to run
 # suggest trying it first in a virtual environment
 # will ask for input of a url
+# class example was url = 'https://www.youtube.com/watch?v=LStXdttFj_o'
 
 import pytube
 
