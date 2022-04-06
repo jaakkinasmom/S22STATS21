5ec4cf559752e048110fd2d821f9763173ad486f
lewv <23514785+lewv@users.noreply.github.com>
2022-04-06 15:08:37 -0700

    Add files via upload

---
+++ examples/Currency2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+# this one is just a super simple example
+
+print("Please input a dollar amount (no decimals):")
+
+dollars = input()
+
+yen = int(dollars) * 123
+
+print(f"Your {dollars} dollars is approximately {yen} yen.")
